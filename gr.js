663ec4f559727ed4ce0df91e78729ed491d0da6f
Kalin Bowden <kalinbowden@gmail.com>
2017-10-13 19:09:54 -0500

    ponies

--- gr.js
+++ gr.js
@@ -6,11 +6,11 @@ Date:
 // Class level variables
 var formValidates = true;
 var pony = ['0', '1', '2', '3', '4', '5', '6'];
-var names = ['Rainbow Dash', 'Flutter Shy', 'Derpy Whooves', 'Dr. Whooves', 'Scootaloo', 'Sweetie Belle', 'Discord'];
-var ponysSelect = ['images/racer0', 'images/racer1', 'images/racer2', 'images/racer3','images/racer4','images/racer5','images/racer6'];
-var ponysRacing = ['images/racer0', 'images/racer1', 'images/racer2', 'images/racer3','images/racer4','images/racer5','images/racer6'];
-var ponysWinning = ['images/racer0', 'images/racer1', 'images/racer2', 'images/racer3','images/racer4','images/racer5','images/racer6'];
-var isPegasus = [true, true, true, false, false, false, false];
+var names = ['Rainbow Dash', 'Flutter Shy', 'Derpy Whooves', 'Pinkie Pie', 'Scootaloo', 'Sweetie Belle', 'Twilight Sparkle'];
+var ponysSelect = ['images/select0.gif', 'images/select1.gif', 'images/select2.gif', 'images/select3.gif', 'images/select4.gif', 'images/select5.gif', 'images/select6.gif'];
+var ponysRacing = ['images/racer0.gif', 'images/racer1.gif', 'images/racer2.gif', 'images/racer3.png', 'images/racer4.gif', 'images/racer5.gif', 'images/racer6.gif'];
+var ponysWinning = ['images/racer0.gif', 'images/racer1.gif', 'images/racer2.gif', 'images/racer3.gif', 'images/racer4.gif', 'images/racer5.gif', 'images/racer6.gif'];
+var isPegasus = [true, true, true, false, false, false, true];
 var count0 = 0;
 var count1 = 20;
 var count2 = 0;
@@ -30,47 +30,35 @@ var racers = 2;
 var raceOver = false;
 var racer0;
 var racer1;
+var temp = 100;
 
 
 
-function onLoadMain()
+function onLoadMain() 
 {
-    //
-    var myInt = setInterval(animateHeadser, 5);
+    // Set Interval
+    var myInt = setInterval(animateHeader, 5);
+
+    // Add listners to page
     document.getElementById("btnSignIn").addEventListener("click", gotoSignIn, false);
     document.getElementById("back").addEventListener("click", gotoHeader, false);
     document.getElementById("btnLogIn").addEventListener("click", gotoChar, false);
     document.getElementById("btnPickChar").addEventListener("click", gotoRace, false);
 
-    //
-    var racerImg0 = document.getElementById("test0");
-    var racerImg1 = document.getElementById("test1");
+    // TODO
     var rand = document.getElementById("rand");
-    racerImg0.src = "";
-    racerImg1.src = "";
-    rand.src = "images/random" + Math.floor(Math.random()*9) +".gif";
+    rand.src = "images/random" + Math.floor(Math.random() * 9) + ".gif";
+}
 
 
-    //
-    for (var index = 0; index < pony.length; index++)
-    {
-        pony[index] = createPony(names[index], ponysSelect[index], ponysRacing[index], ponysWinning[index], isPegasus[index]);
-        console.log(pony[index][0].toString(),pony[index][1].toString(), pony[index][2].toString(), pony[index][3].toString());
-    }
-    
-}   
-
-
-function createPony(name, select, race, win, peg, index)
-{
+function createPony(name, select, race, win, peg, index) {
     var tempPony = [name, select, race, win, peg];
     pony[index] = tempPony;
 }
 
 
 //goto
-function animateHeader()
-{
+function animateHeader() {
     var header = document.getElementById("head");
     var title = document.getElementById("title");
     var leftArrow = document.getElementById("charArrowLeft");
@@ -86,28 +74,28 @@ function animateHeader()
     var winner;
 
 
-    if (isRacing)
-    {
-        if (count4 < 75000 && count4 % 6 === 0 )
-        {
-            raceBack.style.backgroundPositionX = (count4)/15 + "%";
-            raceBack1.style.backgroundPositionX = (count4)/50*-1 + "%";
-            
-            raceCompelted0 += Math.ceil(Math.random()*5);
-            racer0.style.left = (raceCompelted0) + "px";
-            raceCompelted1 += Math.ceil(Math.random()*5);
-            racer1.style.left = (raceCompelted1) + "px";
+    onGameEnd();
+    
 
-            console.log('Racer 0: ' + racer0.style.left.toString());
-            console.log('Racer 1: ' + racer1.style.left.toString());
-            
-        }
-        else if (count4 >= 75000)
+
+    if (isRacing) 
+    {
+        if (count4 < 75000 && count4 % 6 === 0) 
+        {
+            raceBack.style.backgroundPositionX = (count4) / 15 + "%";
+            raceBack1.style.backgroundPositionX = (count4) / 50 * -1 + "%";
+
+            raceCompelted0 += Math.ceil(Math.random() * 5);
+            racer0.style.left = (raceCompelted0) + "px";
+            raceCompelted1 += Math.ceil(Math.random() * 5);
+            racer1.style.left = (raceCompelted1) + "px";
+        } 
+        else if (count4 >= 75000) 
         {
             count4 = 0;
         }
 
-        if (raceCompelted0 > 1100 || raceCompelted1 > 1100)
+        if (raceCompelted0 > 1100 || raceCompelted1 > 1100) 
         {
             count5 += .001;
             finishLine.style.opacity = count5;
@@ -115,107 +103,85 @@ function animateHeader()
         count4++;
     }
 
-    if (count0 < 360)
-    {
+    if (count0 < 360) {
         header.style.background = "linear-gradient(-" + count0 + "deg, purple, orangered)";
         count0 += 2;
-    }
-    else if (count0 >= 360)
-    {
+    } else if (count0 >= 360) {
         count0 = 0;
         header.style.background = "linear-gradient(-" + count0 + "deg, purple, orangered)";
     }
 
     //
-    if (count6 < 256)
-    {
+    if (count6 < 256) {
         count6++;
-    }
-    else
-    {
+    } else {
         count6 = 0;
     }
 
     //
-    
+
 
     //
-    if (count8 > 0)
-    {
+    if (count8 > 0) {
         count8++;
-    }
-    else
-    {
+    } else {
         count8 = 255;
     }
 
-    leftArrow.style.color = 'rgb('+ count6 +','+ count7 +','+ count8 +')';
-    rightArrow.style.color = 'rgb('+ count6 +','+ count7 +','+ count8 +')';
+    leftArrow.style.color = 'rgb(' + count6 + ',' + count7 + ',' + count8 + ')';
+    rightArrow.style.color = 'rgb(' + count6 + ',' + count7 + ',' + count8 + ')';
 
 
 
     //
-    if (count1 < 21 && isGrowing)
-    {
+    if (count1 < 21 && isGrowing) {
         count1 += .015;
         title.style.fontSize = count1 + "em"
     }
-    
-    if (count1 > 21 && isGrowing)
-    {
+
+    if (count1 > 21 && isGrowing) {
         isGrowing = false
     }
 
-    if ( count1 > 19 && !isGrowing)
-    {
+    if (count1 > 19 && !isGrowing) {
         count1 -= .015;
         title.style.fontSize = count1 + "em"
     }
 
 
-    if ( count1 < 19 && !isGrowing)
-    {
+    if (count1 < 19 && !isGrowing) {
         isGrowing = true;
     }
 
-    if (count2 < 1500)
-    {
+    if (count2 < 1500) {
         count2 += 1;
-    }
-    else if (count2 >= 1500)
-    {
-        if (count3 < 360)
-        {
+    } else if (count2 >= 1500) {
+        if (count3 < 360) {
             count3 += 1;
             title.style.transform = "rotate(" + -count3 + "deg)";
-            
-        }
-        else if(count3 >= 360)
-        {
+
+        } else if (count3 >= 360) {
             count3 = 0;
             count2 = 0;
         }
-        
+
     }
 
 
     //
-    if( (raceCompelted0 > 1635 || raceCompelted1 > 1635) && !raceOver)
-    {
+    if ((raceCompelted0 > 1635 || raceCompelted1 > 1635) && !raceOver) {
         raceOver = !raceOver;
 
     }
 
-    if(winner)
-    {
-        //winnerIcon.src = 
+    if (winner) {
+        
     }
 
 }
 
 
-function gotoHeader()
-{
+function gotoHeader() {
     var header = document.getElementById("head");
     var signIn = document.getElementById("signInForm");
 
@@ -224,8 +190,7 @@ function gotoHeader()
 }
 
 
-function gotoSignIn()
-{
+function gotoSignIn() {
     var header = document.getElementById("head");
     var race = document.getElementById("race");
     var race1 = document.getElementById("race1");
@@ -234,11 +199,10 @@ function gotoSignIn()
     race.style.display = "block";
     race1.style.display = "block";
     race2.style.display = "block";
-    
+
 }
 
-function gotoChar()
-{
+function gotoChar() {
     var charSelect = document.getElementById("charSelect");
     var signIn = document.getElementById("signInForm");
 
@@ -247,8 +211,7 @@ function gotoChar()
 }
 
 
-function gotoRace()
-{
+function gotoRace() {
     var race = document.getElementById("race");
     var charSelect = document.getElementById("charSelect");
 
@@ -256,90 +219,103 @@ function gotoRace()
     charSelect.style.display = "none";
 }
 
-function toggleRace()
-{
+function toggleRace() {
     var light = document.getElementById("go");
     isRacing = !isRacing;
 
-    if (isRacing)
-    {
+    if (isRacing) {
         light.style.background = 'radial-gradient(greenyellow,green)';
-    }
-    else
-    {
+    } else {
         light.style.background = 'radial-gradient(lightcoral,red)'
     }
 }
 
-
-function navRight()
+// In the select screen navigate right
+function navRight() 
 {
+    // Function level variables
     var char = document.getElementById("char0");
     var name = document.getElementById("charName");
 
-    if(currentIndex === 6)
-    {
+    // If the current index exceeds the max set index back to 0
+    if (currentIndex === 6) {
         currentIndex = 0;
-    }
-    else
-    {
+    } else {
         currentIndex++
     }
 
-
-    char.src = 'images/racer' + currentIndex + '.gif';
+    // Display the new pony and title
+    char.src = ponysSelect[currentIndex];;
     name.innerHTML = names[currentIndex];
 }
 
-function navLeft()
+
+// In the select screen navigate left
+function navLeft() 
 {
+    // function level variables
     var char = document.getElementById("char0");
     var name = document.getElementById("charName");
 
-    if(currentIndex === 0)
-    {
+    // If the current index exceeds the max set index back to 0
+    if (currentIndex === 0) {
         currentIndex = 6;
-    }
-    else
-    {
+    } else {
         currentIndex--
     }
 
-    char.src = 'images/racer' + currentIndex + '.gif';
+    // Display the new pony and title
+    char.src = ponysSelect[currentIndex];
     name.innerHTML = names[currentIndex];
 }
 
-function grabRacer()
+function grabRacer() 
 {
-    var temp = pony[currentIndex];
+    // Function level variables
     var raceImg0 = document.getElementById("test0");
     var raceImg1 = document.getElementById("test1");
+    var opponetPony = currentIndex;
 
-    alert(temp[0].toString(), temp[1].toString(), temp[0].toString(), temp[2].toString(),temp[3].toString(), temp[4].toString());
-    
-    if(temp[4])
+    // If user selects a pegesus pick an non pegesus
+    if (isPegasus[currentIndex]) 
     {
-        racer0 = temp;
-
-        while(temp[4])
+        while (isPegasus[opponetPony]) 
         {
-            temp = pony[Math.ceil(Math.random()*7)];
+            opponetPony = Math.floor(Math.random() * 7);
         }
-        racer1 = temp;
-    }
-    else
+
+        // Set pegasus to sky
+        raceImg0.src = ponysRacing[currentIndex];
+        raceImg1.src = ponysRacing[opponetPony];
+    } 
+    else 
     {
-        racer1 = temp;
-
-        while(!temp[4])
+        while (!isPegasus[opponetPony]) 
         {
-            temp = pony[Math.ceil(Math.random()*7)];
+            opponetPony = Math.floor(Math.random() * 7);
         }
-        racer0 = temp;
-    }
 
-    raceImg0.src = racer0[2];
-    raceImg1.src = racer1[2];
+        // Set pegasus to sky
+        raceImg0.src = ponysRacing[opponetPony];
+        raceImg1.src = ponysRacing[currentIndex];
+    }
+}
+
+
+function onWin()
+{
+
+}
+
+function onGameEnd()
+{
+    // Function level variables
+    var gameScreen0 = document.getElementById('race1');
+    var gameScreen1 = document.getElementById('race2');
+    temp -= 1;
+
+    gameScreen0.style.opacity = temp + '%'; 
+    gameScreen1.style.opacity = temp + '%';
 }
 
 window.addEventListener("load", onLoadMain, false);
